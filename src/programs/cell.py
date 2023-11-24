53de8e468efe6601a4a10179118a037a4a8a09d1
Clovis <paccou.clovis@gmail.com>
2023-11-24 21:45:53 +0100

    optimisations in Cell.__init__

--- src/programs/cell.py
+++ src/programs/cell.py
@@ -7,20 +7,14 @@ class Cell:
         if type(value) != int:
             raise TypeError(f"The `value` argument must be an integer (type : {type(value)})")
         
-        if type(state) != str:
-            raise TypeError(f"The `state` argument must be a string (type : {type(state)})")
-        
         if value < 0 or value > 9:
             raise ValueError(f"The `value` argument must be between 0 and 9 (value : {value})")
         
-        if not state in ["unlocked", "locked", "superlocked"]:
-            raise ValueError(
-                f"The `state` argument must be \"unlocked\", \"locked\" or \"superlock\" (value : {state})")
-        
         self.value = value  # entre 0 et 9 (0 = case vide)
-        self.state = state  # valeurs possibles: unlocked, locked, superlocked
+        self.state = self.set_state(state)  # valeurs possibles: unlocked, locked, superlocked
         self.text = None
-        self.set_text(text='0', font_size=15, color=(0, 0, 0))
+        self.font_size = 30
+        self.set_text(text='0', color=(0, 0, 0))
     
     def get_value(self) -> int:
         """
@@ -47,8 +41,9 @@ class Cell:
         if value < 0 or value > 9:
             raise ValueError(f"The `value` argument must be between 0 and 9 (value : {value})")
         
+            
         self.value = value
-        self.set_text(str(self.value), 15, (0, 0, 0))
+        self.set_text(str(self.value), (0, 0, 0))
 
     
     def set_state(self, state: str):
@@ -64,13 +59,13 @@ class Cell:
         
         self.state = state
     
-    def set_text(self, text: str, font_size: int, color: tuple[int, int, int]) -> None:
+    def set_text(self, text: str, color: tuple[int, int, int]) -> None:
         """
         Permet de créer un texte pour afficher la valeur de la cellule
         :param text: texte à afficher
         :param font_size: taille de la police
         :param color: couleur du texte (RGB)
         """
-        font = pygame.font.SysFont('Calibri', font_size)  # création d'une police pour afficher les chiffres
+        font = pygame.font.SysFont('Calibri', self.font_size)  # création d'une police pour afficher les chiffres
         self.text = font.render(text, True, color)  # création du texte (self.text est une Surface)
         
