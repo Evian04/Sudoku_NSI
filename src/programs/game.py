62027e4b1b5507c3753d0fb5c1fab46fe4f5e0a8
Evan <evan.b.portrait@gmail.com>
2023-11-24 15:59:24 +0100

    scale grid image

--- src/programs/game.py
+++ src/programs/game.py
@@ -8,7 +8,7 @@ class Game:
         self.screen = screen
         self.sudoku = Sudoku(self)
         
-        self.grid_image = pygame.image.load("src/graphics/grid.png")
+        self.grid_image = pygame.transform.scale(pygame.image.load("src/graphics/grid.png"), (self.screen.get_height() - 10, self.screen.get_height() - 10))
         
         self.sudoku = Sudoku(self)
         
