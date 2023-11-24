632b70dbeec89d060d3484b97d618121e74bc573
Evan <evan.b.portrait@gmail.com>
2023-11-24 16:57:42 +0100

    Sélection v2

--- src/programs/game.py
+++ src/programs/game.py
@@ -19,24 +19,54 @@ class Game:
                 self.grid_image_rect.y + self.grid_image_rect.height / 9 * y,
                 self.grid_image_rect.width / 9,
                 self.grid_image_rect.height / 9
-                )
-            for y in range(9)] for x in range(9)]
+            )
+        for y in range(9)] for x in range(9)]
         
     def update(self, all_events: list[pygame.event.Event]):
         """
         Exécute les actions nécessaires au bon fonctionnement du jeu
         """
+        
         pygame.draw.rect(self.screen, self.background_color, self.screen.get_rect())
         
         self.display_elements()
         
-        for x in range(9):
-            for y in range(9):
-                if self.all_rect[x][y].collidepoint(pygame.mouse.get_pos()):
-                    pygame.draw.rect(self.screen, (100, 100, 100), self.all_rect[x][y])
-        
-        for event in all_events: pass
+        for event in all_events:
+            
+            if event.type == pygame.KEYDOWN:
+                
+                if event.key == pygame.K_ESCAPE:
+                    self.sudoku.deselect_cell()
+                    
+                if event.key == pygame.K_LEFT:
+                    if self.sudoku.selected_cell != [-1, -1]:
+                        if self.sudoku.selected_cell[0] > 0:
+                            self.sudoku.selected_cell[0] -= 1
+                
+                if event.key == pygame.K_RIGHT:
+                    if self.sudoku.selected_cell != [-1, -1]:
+                        if self.sudoku.selected_cell[0] < 8:
+                            self.sudoku.selected_cell[0] += 1
+                    
+                if event.key == pygame.K_UP:
+                    if self.sudoku.selected_cell != [-1, -1]:
+                        if self.sudoku.selected_cell[1] > 0:
+                            self.sudoku.selected_cell[1] -= 1
+                
+                if event.key == pygame.K_DOWN:
+                    if self.sudoku.selected_cell != [-1, -1]:
+                        if self.sudoku.selected_cell[1] < 8:
+                            self.sudoku.selected_cell[1] += 1
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for x in range(9):
+                    for y in range(9):
+                        if self.all_rect[x][y].collidepoint(pygame.mouse.get_pos()):
+                            self.sudoku.select_cell(x, y)
     
     def display_elements(self):
         
         self.screen.blit(self.grid_image, self.grid_image_rect)
+
+        if self.sudoku.selected_cell != [-1, -1]:
+            pygame.draw.rect(self.screen, (80, 80, 80), self.all_rect[self.sudoku.selected_cell[0]][self.sudoku.selected_cell[1]])
--- src/programs/sudoku.py
+++ src/programs/sudoku.py
@@ -7,8 +7,26 @@ class Sudoku:
     def __init__(self, game):
         self.game = game
         self.grid = Grid()
+        
+        self.selected_cell = [-1, -1] # Coordonnées de la case sélectionnée, [-1, -1] si aucune case
     
-    def set_grid(self, new_grid): pass
+    def select_cell(self, x: int, y: int):
+        """
+        Selectionne la case de coordonnées (x, y)
+        """
+        
+        # TEST
+        
+        self.selected_cell = [x, y]
+        
+    def deselect_cell(self):
+        """
+        Déselectionne la case sélectionnée
+        """
+        
+        self.selected_cell = [-1, -1]
+    
+    def load_grid(self, new_grid): pass
         
     def lock_cell(self, x: int, y: int): pass
     
