26a0c12bf1344d5f0c2de23af29f89887195886f
Evan <evan.b.portrait@gmail.com>
2023-11-30 22:54:34 +0100

    ajout de commentaires dans quelques fonctions de sudoku.py

--- src/programs/sudoku.py
+++ src/programs/sudoku.py
@@ -16,8 +16,10 @@ class Sudoku:
         Selectionne la case de coordonnées (x, y)
         """
         
+        # Test des préconditions
         test_errors(coordinates = coordinates)
         
+        # Code de la fonction
         self.selected_cell = coordinates
     
     def deselect_cell(self):
@@ -25,6 +27,7 @@ class Sudoku:
         Dessélectionne la case sélectionnée
         """
         
+        # Code de la fonction
         self.selected_cell = (-1, -1)
     
     def move_selected_cell(self, direction: str):
@@ -32,48 +35,62 @@ class Sudoku:
         Déplace les coordonnées de la case sélctionnées vers la direction indiquée
         """
         
-        if type(direction) != str:
-            raise ValueError(f"The `direction` argument must be a string (type : {type(direction)})")
+        # Test des préconditions
+        assert type(direction) == str, f"The `direction` argument must be a string (type : {type(direction)})"
+        assert direction in ["left", "right", "up", "down"], f'The `direction` argument must be "left", "right", "up" or "down" (value : {direction})'
         
+        # Code de la fonction
         match direction:
             
             case "left":
+                # Si l'utilisateur demande de déplacer la sélection vers la gauche, mais qu'elle est déjà au maximum de cette direction
                 if self.selected_cell[0] == 0:
+                    # Alors ne rien faire
                     return
                 
+                # Sinon déplacer la sélection d'un cran vers la gauche
                 self.selected_cell = (self.selected_cell[0] - 1, self.selected_cell[1])
             
             case "right":
+                # Si l'utilisateur demande de déplacer la sélection vers la droite, mais qu'elle est déjà au maximum de cette direction
                 if self.selected_cell[0] == 8:
+                    # Alors ne rien faire
                     return
             
+                # Sinon déplacer la sélection d'un cran vers la droite
                 self.selected_cell = (self.selected_cell[0] + 1, self.selected_cell[1])
             
             case "up":
+                # Si l'utilisateur demande de déplacer la sélection vers le haut, mais qu'elle est déjà au maximum de cette direction
                 if self.selected_cell[1] == 0:
+                    # Alors ne rien faire
                     return
                 
+                # Sinon déplacer la sélection d'un cran vers le haut
                 self.selected_cell = (self.selected_cell[0], self.selected_cell[1] - 1)
             
             case "down":
+                # Si l'utilisateur demande de déplacer la sélection vers le bas, mais qu'elle est déjà au maximum de cette direction
                 if self.selected_cell[1] == 8:
+                    # Alors ne rien faire
                     return
                 
+                # Sinon déplacer la sélection d'un cran vers le bas
                 self.selected_cell = (self.selected_cell[0], self.selected_cell[1] + 1)
-                
-            case other:
-                raise ValueError(f'The `direction` argument must be "left", "right", "up" or "down" (value : {direction})')
+
+        # Test des postconditions
+        test_errors(coordinates = self.selected_cell)
     
     def set_selected_cell_value(self, value: int):
         """
         Met la valeur de la case sélectionnée à `value`
         """
         
-        test_errors(value = value)
+        # Test des préconditions
+        assert self.selected_cell != (-1, -1), "You must select a cell in order to set its value"
+        test_errors(coordinates = self.selected_cell, value = value)
         
-        if self.selected_cell == (-1, -1):
-            raise ValueError("You must select a cell in order to set its value")
-        
+        # Code de la fonction
         self.grid.set_cell_value(self.selected_cell, value)
         
     def set_selected_cell_color(self, color: tuple[int, int, int]):
@@ -81,9 +98,11 @@ class Sudoku:
         Met la couleur de la case sélectionnée à `color`
         """
         
+        # Test des préconditions
         test_errors(color = color)
         
-        self.grid.set_cell_color(self.selected_cell, color)
+        # Code de la fonction
+        self.grid.set_cell_color(self.selected_cell, color) 
     
     def load_grid(self):
         """
@@ -96,53 +115,73 @@ class Sudoku:
             initialfile="model.sdk",
             filetypes=[("Sudoku file", "*.sdk")]
         )
+        
+        # Dans le cas où l'utilisateur n'a pas sélectionné de fichier
         if not file_path:
+            # Affiche un message en console et ne fait rien d'autre
             print("No file selected for opening, Empty grid...")
             return
         
+        # Récupère le contenu du fichier sélectionné
         with open(file_path, "rt") as file:
             file_content = file.read()
         
+        # Sépare les valeurs et les états des cases dans deux sous-listes
         file_content = file_content.split("\n\n")
         
-        cell_values = [[int(value) for value in line] for line in file_content[0].split("\n")]
+        # Formate la liste des valeurs au format list[list[int]]
+        list_values = [[int(value) for value in line] for line in file_content[0].split("\n")]
         
+        # Formate la liste des états au format list[list[str]]
         convert_state = {"0": "unlocked", "1": "locked", "2": "superlocked"}
-        cell_states = [[convert_state.get(state, 0) for state in line] for line in file_content[1].split("\n")]
+        list_states = [[convert_state.get(state, 0) for state in line] for line in file_content[1].split("\n")]
         
-        self.grid.set_content(cell_values, cell_states)
+        # Test postconditions
+        test_errors(list_values = list_values, list_states = list_states)
+        
+        # Remplace le contenu de la grille par le contenu lu dans le fichier
+        self.grid.set_content(list_values, list_states)
     
     def save_grid(self):
         """
         /!\ Enregistre la grille actuelle dans un fichier Avec ou sans les valeurs saisies par utilisateur ?
         """
-        filepath = asksaveasfilename(initialdir="src/save_folder/", initialfile='saved_sudoku.sdk',
-                                     filetypes=[("Sudoku file", "*.sdk")])
+        
+        # Récupère le chemin du fichier à sauvegarder
+        filepath = asksaveasfilename(
+            initialdir="src/save_folder/",
+            initialfile='saved_sudoku.sdk',
+            filetypes=[("Sudoku file", "*.sdk")]
+        )
+        
+        # Si l'utilisateur n'a rien entré
         if not filepath:
+            # Afficher un message dans la console et ne rien faire d'autre
             print('No file selected for saving')
             return False
         
-        # VALEURS CELLULES:
         # convertit toutes les valeurs des cellules en string
-        double_list_values = [[str(cell.value) for cell in line]
-                               for line in self.grid.content]
+        double_list_values = [[str(cell.value) for cell in line] for line in self.grid.content]
+        
         # convertit la double liste en liste simple (une string par linee)
         list_values = ["".join(line) for line in double_list_values]
+        
         #convertit la liste en une string
         str_content = "\n".join(list_values)
 
-        # ETATS CELLULES:
-        convert_state = {"unlocked": "0", "locked": "1", "superlocked": "2"}
+        convert_state = {"unlocked": "0", "locked": "1", "superlocked": "2"} # Dictionnaire de conversion
         
         # convertit toutes les valeurs des celules en string avec le dictionnaire de conversion
-        double_list_state = [[convert_state[cell.state] for cell in line]
-                               for line in self.grid.content]
+        double_list_state = [[convert_state[cell.state] for cell in line] for line in self.grid.content]
+        
         # convertit la double liste en liste simple (une string par ligne)
         list_state = ["".join(line) for line in double_list_state]
         
-        # additionne les deux (valuers, états)
-        str_content += "\n\n" + "\n".join(list_state)  # "\n\n" pour séparer les valeurs grille de leur correspondance avec les êtats des cases
-        with open(filepath,"w") as file:
+        # additionne les deux (valeurs, états)
+        str_content += "\n\n" + "\n".join(list_state)  # "\n\n" pour séparer les valeurs et les états des cases
+        
+        # écrit le contenu obtenu dans un fichier
+        with open(filepath, "w") as file:
             file.write(str_content)
     
     def lock_selected_cell(self):
@@ -150,12 +189,16 @@ class Sudoku:
         Verrouille la case sélectionnée, la valeur de la case ne pourra plus être modifiée
         """
         
-        if self.selected_cell == (-1, -1):
-            raise ValueError("You must select a cell in order to lock it")
+        # Test de préconditions
+        assert self.selected_cell != (-1, -1), "You must select a cell in order to lock it"
         
+        # Code de la fonction
+        # Si la case sélectionnée est "superlocked"
         if self.grid.get_cell_state(self.selected_cell) == "superlocked":
+            # Ne rien faire
             return
         
+        # Sinon, mettre l'état de la case sélectionnée à "locked"
         self.grid.set_cell_state(self.selected_cell, "locked")
     
     def unlock_selected_cell(self):
@@ -163,12 +206,17 @@ class Sudoku:
         Déverrouille la case sélectionnée, la valeur de la case pourra de nouveau être modifiée
         """
         
-        if self.selected_cell == (-1, -1):
-            raise ValueError("You must select a cell in order to unlock it")
+        # Test de préconditions
+        assert self.selected_cell != (-1, -1), "You must select a cell in order to unlock it"
+        test_errors(coordinates = self.selected_cell)
         
+        # Code de la fonction
+        # Si la case sélectionnée est "superlocked"
         if self.grid.get_cell_state(self.selected_cell) == "superlocked":
+            # Ne rien faire
             return
         
+        # Sinon, mettre l'état de la case sélectionnée à "unlocked"
         self.grid.set_cell_state(self.selected_cell, "unlocked")
     
     def is_valid(self) -> bool:
@@ -176,13 +224,17 @@ class Sudoku:
         Renvois True si la grille ne comporte aucune erreurs, et False si elle en comporte au moins une
         """
         
+        # Pour tout les formats de grille possibles (lignes, colonnes et carrés)
         for grid_format in [self.grid.get_content_as(format) for format in ["lines", "columns", "squares"]]:
             for x in range(9):
-                for y in range(9):
+                for n in range(1, 10):
                     
-                    if grid_format[x].count(grid_format[x][y]) > 1:
+                    # Si l'un des sous-groupes de ce format compte plusieur fois le même chiffre
+                    if grid_format[x].count(n) > 1:
+                        # Renvoyer False
                         return False
         
+        # Si aucun doublon n'a été trouvé, renvoyer True
         return True
 
     def verify_cell(self, coordinates: tuple[int, int]):
@@ -191,6 +243,7 @@ class Sudoku:
         Ajoute les couleurs sur le texte
         """
         
+        # Test de préconditions
         test_errors(coordinates = coordinates)
         
         for n in range(1, 10):
